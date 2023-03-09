115ee1b93925b2cff293331f54891dd26b431b4c
taz5 <suraj.deshini007@gmail.com>
2023-03-09 17:21:42 +0100

    Modified

--- docs/search/namespaces_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['go_5fto_5fpoint_33',['go_to_point',['../namespacego__to__point.html',1,'']]]
-];
--- docs/search/namespaces_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['rt2_5fassignment1_34',['rt2_assignment1',['../namespacert2__assignment1.html',1,'']]]
-];
