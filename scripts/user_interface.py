7615fca877a800a0789ccbdeaae154eb381b1ede
taz5 <suraj.deshini007@gmail.com>
2023-03-09 18:41:23 +0100

    Jupyter notebook

--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -1,50 +1,22 @@
-# Import necessary libraries
 import rospy
 import time
 from rt2_assignment1.srv import Command
 
-# Define the main function
 def main():
-    # Initialize the ROS node
     rospy.init_node('user_interface')
-
-    # Create a service client for the 'Command' service
     ui_client = rospy.ServiceProxy('/user_interface', Command)
-
-    # Wait for 10 seconds
     time.sleep(10)
-
-    # Set the loop rate to 20 Hz
     rate = rospy.Rate(20)
-
-    # Display a message for the user interface
     print("\n User interface to control the Robot")
-
-    # Prompt the user to enter '1' to start the robot or '0' to stop it
     x = int(input("\nPress 1 to start the robot from the current position "))
-
-    # Run the loop until the program is stopped
     while not rospy.is_shutdown():
-        if x == 1:
-            # Send the 'start' command to the robot
+        if (x == 1):
             ui_client("start")
-
-            # Prompt the user to enter '0' to stop the robot
-            x = int(input("\nPress 0 to stop the robot at the last position"))
+            x = int(input("\nPress 0 to stop the robot  at the last position"))
         else:
-            # Display a message indicating that the robot is stopping
             print("Please wait, the robot is going to stop when the position will be reached")
-
-            # Send the 'stop' command to the robot
             ui_client("stop")
-
-            # Prompt the user to enter '1' to start the robot again
             x = int(input("\nPress 1 to start the robot from the current position "))
-
-        # Sleep for a short time to maintain the loop rate
-        rate.sleep()
-
-# Run the main function if the script is executed directly
+            
 if __name__ == '__main__':
     main()
-
--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -1,9 +1,12 @@
 #include "ros/ros.h"
 #include "rt2_assignment1/Command.h"
-#include "rt2_assignment1/Position.h"
 #include "rt2_assignment1/RandomPosition.h"
+#include "rt2_assignment1/RcAction.h"
+#include "actionlib/client/simple_action_client.h"
+#include "actionlib/client/terminal_state.h"
 
 bool start = false;
+
 
 bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Command::Response &res){
     if (req.command == "start"){
@@ -15,33 +18,52 @@ bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Com
     return true;
 }
 
-
 int main(int argc, char **argv)
 {
    ros::init(argc, argv, "state_machine");
    ros::NodeHandle n;
    ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
    ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
-   ros::ServiceClient client_p = n.serviceClient<rt2_assignment1::Position>("/go_to_point");
+   actionlib::SimpleActionClient <rt2_assignment1::RcAction> pos("/go_to_point", true);
    
+   rt2_assignment1::RcGoal gp;
    rt2_assignment1::RandomPosition rp;
    rp.request.x_max = 5.0;
    rp.request.x_min = -5.0;
    rp.request.y_max = 5.0;
    rp.request.y_min = -5.0;
-   rt2_assignment1::Position p;
-   
+
    while(ros::ok()){
    	ros::spinOnce();
    	if (start){
    		client_rp.call(rp);
-   		p.request.x = rp.response.x;
-   		p.request.y = rp.response.y;
-   		p.request.theta = rp.response.theta;
-   		std::cout << "\nGoing to the position: x= " << p.request.x << " y= " <<p.request.y << " theta = " <<p.request.theta << std::endl;
-   		client_p.call(p);
-   		std::cout << "Position reached" << std::endl;
-   	}
-   }
-   return 0;
+        
+        gp.x=rp.response.x; 
+        gp.y=rp.response.y;  
+        gp.theta=rp.response.theta;
+
+        std::cout << "\nGoing to the position: x= " << gp.x << "\nGoing to the position: y= " <<gp.y << " \nGoing to the position:theta = " <<gp.theta << std::endl;
+        pos.sendGoal(gp);
+        
+        while (true)
+        {
+           ros::spinOnce();
+           if(start==false)
+           {
+                pos.cancelGoal();
+                    std::cout << "\n currently goal cancelled" << std::endl;
+                    break;
+                }
+                else
+                {
+                    if (pos.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+                    {
+                        std::cout << "\ncurrently Goal Reached!" << std::endl;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
 }
