340939a51353fc90140c090a617166015c6a79da
taz5 <44135346+taz5@users.noreply.github.com>
2023-03-09 15:28:24 +0000

    Modified

--- scripts/go_to_point.py
+++ scripts/go_to_point.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python
 
 
-import rospy
 from geometry_msgs.msg import Twist, Point
 from nav_msgs.msg import Odometry
 from tf import transformations
-from rt2_assignment1.srv import Position
+
+import rospy
+import actionlib
+import rt2_assignment1.msg
 import math
 
 # robot state variables
@@ -14,6 +16,7 @@ yaw_ = 0
 position_ = 0
 state_ = 0
 pub_ = None
+robot_action_server=None
 
 # parameters for control
 yaw_precision_ = math.pi / 9  # +/- 20 degree allowed
@@ -24,6 +27,7 @@ kp_d = 0.2
 ub_a = 0.6
 lb_a = -0.5
 ub_d = 0.6
+
 
 def clbk_odom(msg):
     global position_
@@ -67,7 +71,6 @@ def fix_yaw(des_pos):
     pub_.publish(twist_msg)
     # state change conditions
     if math.fabs(err_yaw) <= yaw_precision_2_:
-        #print ('Yaw error: [%s]' % err_yaw)
         change_state(1)
 
 
@@ -83,18 +86,14 @@ def go_straight_ahead(des_pos):
         twist_msg = Twist()
         twist_msg.linear.x = 0.3
         if twist_msg.linear.x > ub_d:
-            twist_msg.linear.x = ub_d
+           twist_msg.linear.x = ub_d
 
         twist_msg.angular.z = kp_a*err_yaw
         pub_.publish(twist_msg)
-    else: # state change conditions
-        #print ('Position error: [%s]' % err_pos)
+    else: 
         change_state(2)
-
-    # state change conditions
     if math.fabs(err_yaw) > yaw_precision_:
-        #print ('Yaw error: [%s]' % err_yaw)
-        change_state(0)
+      change_state(0)
 
 def fix_final_yaw(des_yaw):
     err_yaw = normalize_angle(des_yaw - yaw_)
@@ -109,7 +108,6 @@ def fix_final_yaw(des_yaw):
     pub_.publish(twist_msg)
     # state change conditions
     if math.fabs(err_yaw) <= yaw_precision_2_:
-        #print ('Yaw error: [%s]' % err_yaw)
         change_state(3)
         
 def done():
@@ -117,32 +115,78 @@ def done():
     twist_msg.linear.x = 0
     twist_msg.angular.z = 0
     pub_.publish(twist_msg)
-    
-def go_to_point(req):
-    desired_position = Point()
-    desired_position.x = req.x
-    desired_position.y = req.y
-    des_yaw = req.theta
+
+def go_to_point(goal):
+    # Declare global variables.
+    global robot_action_server
+    global state_
+
+    # Create a Point object and set its values.
+    desired_position = Point(x=goal.x, y=goal.y)
+
+    # Set the desired yaw angle.
+    des_yaw = goal.theta
+
+    # Change the state to "0".
     change_state(0)
+
+    # Create instances of the RcFeedback and RcResult message types.
+    feedback = rt2_assignment1.msg.RcFeedback()
+    result = rt2_assignment1.msg.RcResult()
+
+    # Enter an infinite loop.
     while True:
-    	if state_ == 0:
-    		fix_yaw(desired_position)
-    	elif state_ == 1:
-    		go_straight_ahead(desired_position)
-    	elif state_ == 2:
-    		fix_final_yaw(des_yaw)
-    	elif state_ == 3:
-    		done()
-    		break
+        # Check if a preempt request has been received.
+        if robot_action_server.is_preempt_requested():
+            # Print a message and call the done() function.
+            print("Preempted the goal")
+            done()
+            # Update the state to "3".
+            state_ = 3
+
+        # Check the value of the state variable and call the corresponding function.
+        if state_ == 0:
+            fix_yaw(desired_position)
+        elif state_ == 1:
+            go_straight_ahead(desired_position)
+        elif state_ == 2:
+            fix_final_yaw(des_yaw)
+        elif state_ == 3:
+            # Call the done() function and break out of the loop.
+            done()
+            break
+
+    # Set the result and mark the goal as succeeded.
+    robot_action_server.set_succeeded(result)
+
+    # Return True to indicate success.
     return True
 
+                
+        
+
+
 def main():
+    # Declare global variables.
     global pub_
+    global robot_action_server
+
+    # Initialize the ROS node.
     rospy.init_node('go_to_point')
+
+    # Create a publisher to send Twist messages.
     pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+
+    # Create a subscriber to receive Odometry messages.
     sub_odom = rospy.Subscriber('/odom', Odometry, clbk_odom)
-    service = rospy.Service('/go_to_point', Position, go_to_point)
+
+    # Create an action server to handle requests to go to a point.
+    robot_action_server = actionlib.SimpleActionServer('/go_to_point', rt2_assignment1.msg.GoToPointAction, execute_cb=go_to_point, auto_start=False)
+    robot_action_server.start()
+
+    # Enter a loop to keep the node running.
     rospy.spin()
+
 
 if __name__ == '__main__':
     main()
--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -1,21 +1,50 @@
+# Import necessary libraries
 import rospy
 import time
 from rt2_assignment1.srv import Command
 
+# Define the main function
 def main():
+    # Initialize the ROS node
     rospy.init_node('user_interface')
+
+    # Create a service client for the 'Command' service
     ui_client = rospy.ServiceProxy('/user_interface', Command)
+
+    # Wait for 10 seconds
     time.sleep(10)
+
+    # Set the loop rate to 20 Hz
     rate = rospy.Rate(20)
-    x = int(input("\nPress 1 to start the robot "))
+
+    # Display a message for the user interface
+    print("\n User interface to control the Robot")
+
+    # Prompt the user to enter '1' to start the robot or '0' to stop it
+    x = int(input("\nPress 1 to start the robot from the current position "))
+
+    # Run the loop until the program is stopped
     while not rospy.is_shutdown():
-        if (x == 1):
+        if x == 1:
+            # Send the 'start' command to the robot
             ui_client("start")
-            x = int(input("\nPress 0 to stop the robot "))
+
+            # Prompt the user to enter '0' to stop the robot
+            x = int(input("\nPress 0 to stop the robot at the last position"))
         else:
+            # Display a message indicating that the robot is stopping
             print("Please wait, the robot is going to stop when the position will be reached")
+
+            # Send the 'stop' command to the robot
             ui_client("stop")
-            x = int(input("\nPress 1 to start the robot "))
-            
+
+            # Prompt the user to enter '1' to start the robot again
+            x = int(input("\nPress 1 to start the robot from the current position "))
+
+        # Sleep for a short time to maintain the loop rate
+        rate.sleep()
+
+# Run the main function if the script is executed directly
 if __name__ == '__main__':
     main()
+
